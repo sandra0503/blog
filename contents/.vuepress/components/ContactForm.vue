7015d191c690c3123180f3824fdfb607a19f7ac3
Sandra Zollner <sandra.zollner@askattest.com>
2020-10-03 13:32:54 +0200

    fix form

--- contents/.vuepress/components/ContactForm.vue
+++ contents/.vuepress/components/ContactForm.vue
@@ -10,19 +10,19 @@
       <label class="input-label md:max-w-32" for="name">{{
         data.contact.nameLabel
       }}</label>
-      <input type="text" id="name" class="input-field" />
+      <input type="text" name="name" class="input-field" />
     </div>
     <div class="form-input items-center">
       <label class="input-label md:max-w-32" for="email">{{
         data.contact.emailLabel
       }}</label>
-      <input type="email" id="email" class="input-field" />
+      <input type="email" name="email" class="input-field" />
     </div>
     <div class="form-input">
       <label class="input-label md:max-w-32" for="message">{{
         data.contact.messageLabel
       }}</label>
-      <textarea id="message" class="input-field h-32 resize-none"></textarea>
+      <textarea name="message" class="input-field h-32 resize-none"></textarea>
     </div>
     <div class="text-center">
       <button class="mt-8 btn-secondary w-32" type="submit">
